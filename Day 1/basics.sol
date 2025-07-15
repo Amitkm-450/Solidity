2b11d233750586c5ccad937aed231b47dcd90cf0
amit kumar <mohapatraamitkumar450@gmail.com>
2025-07-15 23:14:03 +0530

    Basics of solidity

---
+++ Day 1/basics.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier MIT
+pragma solidity ^0.8.30; //Hello I am amit
+
+pragma solidity = 0.8.7 0.9.0
+contract SimpleStorage {
+   boolean , uint, int, address, bytes
+   This gets initialiszed to 0
+   uint256 public favouriteNumber;
+   People public person = People({favouriteNumber 2, name Amit});
+
+   struct People {
+    uint256 favouriteNumber;
+    string name;
+   }
+
+   function store(uint256 _favouriteNumber) public {
+    favouriteNumber = _favouriteNumber;
+    favouriteNumber = favouriteNumber + 1;
+   }
+   
+   function retrive() public view returns(uint256){
+    return favouriteNumber;
+   }
+
+}
