b9d4e746425db1ba70d3dda3a140f9f1900e0f41
amit kumar <mohapatraamitkumar450@gmail.com>
2025-07-15 23:16:07 +0530

    Basics of solidity

--- Day 1/basics.sol
+++ Day 1/basics.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.30; //Hello I am amit
 
 contract SimpleStorage {
-   boolean , uint, int, address, bytes
-   This gets initialiszed to 0
+//    boolean  uint, int, address, bytes
+//    This gets initialiszed to 0
    uint256 public favouriteNumber;
-   People public person = People({favouriteNumber 2, name Amit});
+   People public person = People({favouriteNumber: 2,name: "Amit"});
 
    struct People {
     uint256 favouriteNumber;
